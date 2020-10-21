62442d56296c0a6c9c9640a61061d7ea585725a8
Aline <alinecris@gmail.com>
2020-10-21 22:54:12 +0200

    Add google maps with current browser location

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
 
 function App() {
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+
+  const { REACT_APP_GOOGLE_API_KEY } = process.env
+
+  useEffect(() => {
+    setCurrentLocation()
+  }, [])
+
+  async function setCurrentLocation() {
+    await navigator.geolocation.getCurrentPosition(function (position) {
+      setLatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
+
+    }, function(error) {
+      alert("Enable location to use this App")
+    })
+  }
+
   return (
     <Fragment>
-
+      <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}>
+        <GoogleMap mapContainerStyle={{height: "100vh", width: "100%"}}
+          zoom={15}
+          center={{lat: latitude, lng: longitude}}
+        >
+        </GoogleMap>
+      </LoadScript>
     </Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
