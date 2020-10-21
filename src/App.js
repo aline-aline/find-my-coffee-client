68065c64b02f2a50e405d41020aaca0783920941
Aline <alinecris@gmail.com>
2020-10-21 23:33:48 +0200

    Add api to get the coffeeshops

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment, useState, useEffect } from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+
+import BusinessesService from './services/google_list_of_businesses'
 
 function App() {
   const [latitude, setLatitude] = useState(0)
   const [longitude, setLongitude] = useState(0)
+  const [locations, setLocation] = useState([])
 
   const { REACT_APP_GOOGLE_API_KEY } = process.env
 
@@ -15,10 +18,15 @@ function App() {
     await navigator.geolocation.getCurrentPosition(function (position) {
       setLatitude(position.coords.latitude)
       setLongitude(position.coords.longitude)
-
+      loadCoffeeShops()
     }, function(error) {
       alert("Enable location to use this App")
     })
+  }
+
+  async function loadCoffeeShops() {
+    const response = await BusinessesService.index(latitude, longitude)
+    setLocation(response.data.results)
   }
 
   return (
@@ -28,6 +36,19 @@ function App() {
           zoom={15}
           center={{lat: latitude, lng: longitude}}
         >
+        {
+          locations.map((item, index) => {
+            return (
+              <Marker key={index} icon="/images/coffee-pin.png" title={item.name} animation="4" 
+                position={{lat: item.geometry.location.lat, lng: item.geometry.location.lng}}
+              />
+            )
+          })
+        }
+        <Marker key="my location" icon="/images/my-location-pin.png" title="Your local" animation="2"
+          position={{lat: latitude, lng: longitude}}
+        />
+
         </GoogleMap>
       </LoadScript>
     </Fragment>
